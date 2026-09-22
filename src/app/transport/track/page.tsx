bf83838dd50114a0671f05ef2abf81dc3dc1daa3
hsynfyad11-pixel <hsynfyad11@gmail.com>
2026-09-22 21:45:58 +0300

    Update page.tsx

--- src/app/transport/track/page.tsx
+++ src/app/transport/track/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import React, { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { Navigation, Users, Package, ArrowRight, Loader2, CheckCircle2, ShieldAlert, Phone, Car } from 'lucide-react';
