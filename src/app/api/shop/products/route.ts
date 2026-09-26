83c61414f79d766238d08a023f8c5fc39fcc423a
hsynfyad11-pixel <hsynfyad11@gmail.com>
2026-09-26 11:54:09 +0300

    Update route.ts

--- src/app/api/shop/products/route.ts
+++ src/app/api/shop/products/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export const dynamic = 'force-dynamic';
+
 
 // Add or Edit Product
 export async function POST(request: Request) {
